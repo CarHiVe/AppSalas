44173ecd2cf3bdc31bf41d34fcd97aa720db1d6c
Lozamded <dhiguerafernandez@gmail.com>
2015-06-21 18:32:29 -0300

    migtations

---
+++ database/migrations/2015_06_21_210316_create_campus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampusTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$table->increments('id');
+		$table->string('nombre');
+		$table->string('direccion');
+		$table->double('latitud');
+		$table->double('longitud');
+		$table->string('descripcion');
+		$table->integer('rut_encargado');
+		$table->timestamp('created_at');
+		$table->timestamp('uploated_at');
+
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
